17389476aa0951ec2de60dcfee82b10f91dce299
YevhenVK <onegin797@gmail.com>
2021-10-14 17:11:21 +0300

    Update 00.js

--- js/00.js
+++ js/00.js
@@ -1,13 +1,51 @@
-function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-  let message;
-  // Change code below this line
-  const totalPrice = pricePerDroid * orderedQuantity;
-  const credits = customerCredits - totalPrice;
-    if (customerCredits < totalPrice) {
-      message = "Insufficient funds!";
-    } else { 
-      message = `You ordered ${orderedQuantity} droids, you have ${credits} credits left.`;
-    }
-  // Change code above this line
-  return message;
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   const totalPrice = pricePerDroid * orderedQuantity;
+//   const credits = customerCredits - totalPrice;
+//     if (customerCredits < totalPrice) {
+//       message = "Insufficient funds!";
+//     } else { 
+//       message = `You ordered ${orderedQuantity} droids, you have ${credits} credits left.`;
+//     }
+//   // Change code above this line
+//   return message;
+// }
+
+
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// const threshold = 15;
+
+// // Для чисел меньше чем порог срабатывает continue, выполнение тела прекращается
+// // и управление передаётся на следующую итерацию.
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] < threshold) {
+//     continue;
+//   }
+
+//   console.log(`Число больше чем ${threshold}: ${numbers[i]}`); // 18, 29, 34
+// }
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4.7,
+  price: 5000,
+  tags: ["premium", "promoted", "top", "trusted"],
+  owner: {
+    name: "Henry Sibola",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+
+apartment.area = 60;
+apartment.room = 3;
+apartment.location {
+  location.country = "Jamaica";
+  location.city = "Kingston";
 }
+
+
+// Change code below this line
