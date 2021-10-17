f8acc2f0777faf032e218f2fd981ea53669bde3f
YevhenVK <onegin797@gmail.com>
2021-10-17 14:20:52 +0300

    Update 00.js

--- js/00.js
+++ js/00.js
@@ -26,26 +26,60 @@
 //   console.log(`Число больше чем ${threshold}: ${numbers[i]}`); // 18, 29, 34
 // }
 
-const apartment = {
-  imgUrl: "https://via.placeholder.com/640x480",
-  descr: "Spacious apartment in the city center",
-  rating: 4.7,
-  price: 5000,
-  tags: ["premium", "promoted", "top", "trusted"],
-  owner: {
-    name: "Henry Sibola",
-    phone: "982-126-1588",
-    email: "henry.carter@aptmail.com",
-  },
-};
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
 
 
-apartment.area = 60;
-apartment.room = 3;
-apartment.location {
-  location.country = "Jamaica";
-  location.city = "Kingston";
-}
+// apartment.area = 60;
+// apartment.room = 3;
+// apartment.location {
+//   location.country = "Jamaica";
+//   location.city = "Kingston";
+// }
 
 
 // Change code below this line
+
+
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// // Передаём инлайн функцию greet как колбэк
+// registerGuest("Mango", function greet(name) {
+//   console.log(`Добро пожаловать ${name}.`);
+// });
+
+// // Передаём инлайн функцию notify как колбэк
+// registerGuest("Poly", function notify(name) {
+//   console.log(`Уважаемый(ая) ${name}, ваш номер будет готов через 30 минут.`);
+// });
+
+/////
+
+function makePizza(pizzaName, callback) {
+  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+	callback(pizzaName);
+}
+
+makePizza(" ", function eatPizza(pizzaName) {
+  console.log(`Eating pizza ${pizzaName}.`);
+});
+
+makePizza("Royal Grand", function deliverPizza(pizzaName) {
+  console.log(`Delivering pizza ${pizzaName}.`);
+});
+// Change code below this line
+
+makePizza("Ultracheese");
